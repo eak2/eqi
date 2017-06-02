b4138d8e0ea579a5f2e601f7117e9b1f2808f70a
John <john.douglas@mycostech.com>
2017-06-02 13:03:08 +0700

    update all file support angular meteerail design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import {HttpModule, Http} from '@angular/http';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AuthGuard } from './shared';
+import { MaterialModule } from '@angular/material';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { HammerGestureConfig, HAMMER_GESTURE_CONFIG } from '@angular/platform-browser';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: Http) {
     return ;
+}
+
+export class MyHammerConfig extends HammerGestureConfig  {
+  overrides = <any>{
+      'swipe': {velocity: 0.4, threshold: 20} // override default settings
+  }
 }
 
 @NgModule({
@@ -20,10 +27,13 @@ export function HttpLoaderFactory(http: Http) {
     imports: [
         BrowserModule,
         FormsModule,
+        MaterialModule,
         HttpModule,
-        AppRoutingModule
+        AppRoutingModule, NoopAnimationsModule
     ],
-    providers: [AuthGuard],
+    providers: [AuthGuard,{ provide: HAMMER_GESTURE_CONFIG, 
+                    useClass: MyHammerConfig }],
     bootstrap: [AppComponent]
 })
 export class AppModule { }
+
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './home.component';
+import { MaterialModule } from '@angular/material';
+import { FormControl } from '@angular/forms';
 
 
 @NgModule({
   imports: [
-    CommonModule,
-    HomeRoutingModule
+    CommonModule, 
+    FormsModule,
+    HomeRoutingModule, 
+    ReactiveFormsModule,
+    MaterialModule
   ],
-  declarations: [HomeComponent]
+  declarations: [HomeComponent],
+  bootstrap: [HomeComponent]
 })
 export class HomeModule { }
